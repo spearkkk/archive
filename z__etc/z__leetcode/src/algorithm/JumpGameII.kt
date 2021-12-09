2a13f9c5d886c344bda5d948b6ceaa5168f4888e
spearkkk <spearkkp@gmail.com>
2021-12-09 20:35:49 +0900

    Solve https://leetcode.com/problems/jump-game-ii/

---
+++ z__etc/z__leetcode/src/algorithm/JumpGameII.kt
@@ -0,0 +1,31 @@
+package algorithm
+
+class JumpGameII {
+    private fun doJumping(current: Int, tried: IntArray, nums: IntArray): Int {
+        if (current < 0) {
+            // end
+            return tried[0]
+        }
+
+        val maximum: Int = nums[current]
+
+        if (maximum == 0) {
+            // cannot jump
+            tried[current] = 10001
+            return doJumping(current - 1, tried, nums)
+        }
+
+        val goal: Int = nums.size - 1
+
+        if ((maximum + current) >= goal) {
+            tried[current] = 1
+        } else {
+            tried[current] = ((1 .. maximum).map { current + it }.filter { it < tried.size }.map { tried[it] }.minOrNull() ?: 10001) + 1
+        }
+        return doJumping(current - 1, tried, nums)
+    }
+
+    fun jump(nums: IntArray): Int {
+        return doJumping(nums.size - 2, IntArray(nums.size) { 0 }, nums)
+    }
+}
